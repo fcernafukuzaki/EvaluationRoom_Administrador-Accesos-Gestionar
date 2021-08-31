82b5f08c832f68c2d6c5e76c6ef7230304b7b5d6
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2021-08-31 12:16:45 -0500

    v1.0.0

---
+++ objects/usuario_perfil.py
@@ -0,0 +1,24 @@
+from configs.flask_config import db, ma
+from objects.perfil import Perfil, PerfilSchema
+
+
+class UsuarioPerfil(db.Model):
+    __table_args__ = {"schema": "evaluationroom", 'extend_existing': True}
+    __tablename__ = 'usuarioperfil'
+
+    idperfil = db.Column(db.Integer, db.ForeignKey('evaluationroom.perfil.idperfil'), primary_key=True)
+    idusuario = db.Column(db.Integer, db.ForeignKey('evaluationroom.usuario.idusuario'), primary_key=True)
+
+    perfil = db.relationship('Perfil', 
+                             primaryjoin='and_(UsuarioPerfil.idperfil==Perfil.idperfil)',
+                             order_by="Perfil.idperfil")
+
+class UsuarioPerfilSchema(ma.Schema):
+    class Meta:
+        fields = ('idperfil', 'idusuario')
+
+class UsuarioPerfilInfoSchema(ma.Schema):
+    class Meta:
+        fields = ('idperfil', 'idusuario', 'perfil')
+    
+    perfil = ma.Nested(PerfilSchema)
