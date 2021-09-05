8b19398bd13e658d985a9b73ca2c9f98e3524278
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2021-09-04 20:18:53 -0500

    Develop to main (#3)
    * comit inicial
    
    * v1.0.0
    
    * v1.0.0
    
    * v1.0.0
    
    * v1.0.1
    
    Registrar perfiles de usuario.
    Cambio de formato en respuesta luego de actualizar perfiles de usuario.
    
    * v1.0.1
    
    Registrar perfiles de usuario.
    Cambio de formato en respuesta luego de actualizar perfiles de usuario.

--- objects/usuario_perfil.py
+++ objects/usuario_perfil.py
@@ -6,12 +6,16 @@ class UsuarioPerfil(db.Model):
     __table_args__ = {"schema": "evaluationroom", 'extend_existing': True}
     __tablename__ = 'usuarioperfil'
 
-    idperfil = db.Column(db.Integer, db.ForeignKey('evaluationroom.perfil.idperfil'), primary_key=True)
     idusuario = db.Column(db.Integer, db.ForeignKey('evaluationroom.usuario.idusuario'), primary_key=True)
-
+    idperfil = db.Column(db.Integer, db.ForeignKey('evaluationroom.perfil.idperfil'), primary_key=True)
+    
     perfil = db.relationship('Perfil', 
                              primaryjoin='and_(UsuarioPerfil.idperfil==Perfil.idperfil)',
                              order_by="Perfil.idperfil")
+    
+    def __init__(self, idusuario, idperfil):
+        self.idusuario = idusuario
+        self.idperfil = idperfil
 
 class UsuarioPerfilSchema(ma.Schema):
     class Meta:
